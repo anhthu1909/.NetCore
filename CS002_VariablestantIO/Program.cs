db9d5f032fe6dfa99b704aecafd36c8cfb42db17
AnhThu <nguyenthianhthu09191999@gmail.com>
2021-04-06 20:15:24 +0700

    First commit

---
+++ CS002_VariablestantIO/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace CS002_VariablestantIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string studentName;
+            string address = "Ha Noi, Viet Nam";
+
+            int studentAge;
+            studentAge = 22;
+
+            double so_pi = 3.14;
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Xin chao - Chuong trinh nhap xuat du lieu");
+            Console.ResetColor();
+
+            Console.WriteLine("Gia tri so pi la: ");
+            Console.Write(so_pi);
+            Console.WriteLine();
+
+            int a = 123;
+            double b = 5671.123;
+            Console.WriteLine($"Bien a = {a}, bien b = {b} - tich la {a * b}");
+            
+            char username;
+            Console.Write("Nhap userrname:");
+            username = Console.ReadKey().KeyChar;
+            Console.WriteLine($"Ten nhap vao la : {username}");
+
+            Console.Write("Nhap mot so thuc:");
+            double input = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"So da nhap la:{input}");
+            var bien1 = 3.3;
+            var bien2 = 3;
+            var bien3 = "Bien khai bao voi var";
+            var bien4 = (5<4);
+
+            const string Mon = "Thu hai";
+            Console.WriteLine(Mon);
+        }
+    }
+
+}
---
+++ CS003_VariablestantIO/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CS003_VariablestantIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int number=12;
+           if ((number % 2)==0) 
+                Console.WriteLine($"{number} la so chan");
+        
+            var a = 2;
+            var b = 3.5;
+            var c = 2;
+            var max = a>=b ? a>=c ? a:c:b >= c ?b:c;
+            Console.WriteLine($"{max}");
+
+            int num = 2;
+            switch(num)
+            {
+                case 1:
+                Console.WriteLine("num co gia tri la mot");
+                break;
+                case 2:
+                Console.WriteLine("num co gia tri la hai");
+                break;
+                default:
+                Console.WriteLine("num co gia tri la mot va hai");
+                break;
+            }
+                
+            int so1 = 10;
+            while(so1 <= 9)
+            {
+                Console.WriteLine("so 1: " + so1);
+                so1++;
+            }
+
+            do
+            {
+                Console.WriteLine("so 2:" + so1);
+            }
+            while(so1 < 9);
+        }
+ 
+    }
+}
