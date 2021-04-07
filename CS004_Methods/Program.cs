c8fe58ea78fd59789e3489e7a3f7bb5391168d67
anhthu1909 <nguyenthianhthu09191999@gmail.com>
2021-04-07 10:33:52 +0700

    Add new CS004_methods folder

---
+++ CS004_Methods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CS004_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int kq, kq1;
+            kq = tich(3,4);
+            kq1 = Tinhtoan.tinhtong(3,4);
+            CS004_Methods.Program.xinchao();
+            xinchao();
+            Console.WriteLine("Ket qua a nhan voi b la: {0} va ket qua a cong voi b la: {1}", kq, kq1);
+
+        }
+        static void xinchao()
+        {
+            Console.WriteLine("==================");
+            Console.WriteLine("Xin Chao Anh Thu");
+            Console.WriteLine("==================");
+        }
+
+        public static int tich(int a, int b)
+        {
+            return a*b;
+        }
+    }
+}
---
+++ CS004_Methods/Tinhtoan.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace CS004_Methods
+{
+    class Tinhtoan
+    {
+        public static int tinhtong(int a, int b)
+        {
+            return a+b;
+        }
+    }
+}
+
+
